36b7e108c55c7890bbf141c8d8a6e03175bb1fc4
debian <debian@d-i.lan>
2025-07-11 04:14:45 -0400

    ¸üÐÂ

--- fix.sh
+++ fix.sh
@@ -3,6 +3,6 @@
 
 find content/posts -name "*.md" | while read file; do
     echo "修复: $file"
-    sed -i -E 's#\]\(C:\\Users\\.*\\Documents\\image\\([^)]*)\)#](/blog/\1)#g' "$file"
+    sed -i -E 's#\]\(C:\\Users\\.*\\Documents\\image\\([^)]*)\)#](https://haha077.github.io/blog/\1)#g' "$file"
     sed -i 's#\\#/#g' "$file"
 done
